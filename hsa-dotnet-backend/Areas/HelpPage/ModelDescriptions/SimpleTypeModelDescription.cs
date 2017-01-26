68f3d43749d90549418fb00deb1abf66ff21838a
Pearse Hutson <pearse.2008@gmail.com>
2017-01-26 13:33:16 -0600

    Deleted WebAPI Template Directory

--- hsa-dotnet-backend/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace hsa_dotnet_backend.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- hsa-dotnet-backend/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hsa_dotnet_backend.WebApiApplication" Language="C#" %>
