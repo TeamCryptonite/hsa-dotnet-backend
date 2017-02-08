a0fd095de0dd617512977fd81033f568b7cab680
Pearse Hutson <pearse.2008@gmail.com>
2017-02-08 15:34:55 -0600

    Fixed namespace error in Startup.Auth.cs

--- hsa-dotnet-backend/App_Start/Startup.Auth.cs
+++ hsa-dotnet-backend/App_Start/Startup.Auth.cs
@@ -7,7 +7,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.ActiveDirectory;
 using Owin;
 
-namespace hsa-dotnet-backend
+namespace hsa_dotnet_backend
 {
     public partial class Startup
     {
