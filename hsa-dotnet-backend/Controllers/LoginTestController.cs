cc3bb8ddd2c98409c95777c2e304cef2758b39d4
Pearse Hutson <pearse.2008@gmail.com>
2017-02-08 13:05:08 -0600

    Testing Authentication 01
    Created LoginTestController, Added Authorize directive to DevTest.

--- hsa-dotnet-backend/Controllers/LoginTestController.cs
+++ hsa-dotnet-backend/Controllers/LoginTestController.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
 using System.Web.Http;
 
 namespace hsa_dotnet_backend.Controllers
 {
-    [Authorize]
-    public class DevTestController : ApiController
+    [Route("/logintest/")]
+    public class LoginTestController : ApiController
     {
-        [Route("devtest/")]
-        [HttpGet]
-        public Object DevTest()
+        public Object test()
         {
             return new
             {
-                Test = "First Test",
-                Success = true
+                msg = "Success"
             };
         }
     }
