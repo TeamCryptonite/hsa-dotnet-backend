e258aab99aa5b20f21579c8760c665c044aedcb8
Pearse Hutson <pearse.2008@gmail.com>
2017-02-10 17:17:18 -0600

    Added JsonFormatter so XML isn't returned when not requested.

--- hsa-dotnet-backend/App_Start/WebApiConfig.cs
+++ hsa-dotnet-backend/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
 
 namespace HsaDotnetBackend
 {
@@ -7,6 +8,8 @@ namespace HsaDotnetBackend
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
