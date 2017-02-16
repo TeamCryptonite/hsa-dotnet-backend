f8a23048237de7dabc366639c46754917d825c3e
Pearse Hutson <pearse.2008@gmail.com>
2017-02-16 17:04:23 -0600

    Added ReceiptPictureHelper. Testing in DevtestController

--- hsa-dotnet-backend/Controllers/DevTestController.cs
+++ hsa-dotnet-backend/Controllers/DevTestController.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using System.Linq;
 using System.Web.Http;
 using AutoMapper;
+using HsaDotnetBackend.Helpers;
 using HsaDotnetBackend.Models;
 using HsaDotnetBackend.Models.DTOs;
 using Microsoft.Azure;
@@ -42,25 +43,7 @@ namespace HsaDotnetBackend.Controllers
         [HttpGet]
         public string GetBlob()
         {
-            CloudStorageAccount storageAccount =
-                CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
-
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-            CloudBlobContainer container = blobClient.GetContainerReference("userreceipts");
-
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference("20170216_140140[1].jpg");
-
-            SharedAccessBlobPolicy policy = new SharedAccessBlobPolicy()
-            {
-                SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-5),
-                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(30),
-                Permissions = SharedAccessBlobPermissions.Read
-            };
-
-            string sasBlobToken = blockBlob.GetSharedAccessSignature(policy);
-
-            return blockBlob.Uri + sasBlobToken;
+            return ReceiptPictureHelper.GetReceiptPictureUri("20170216_140140[1].jpg");
         }
     }
 }
---
+++ hsa-dotnet-backend/Helpers/ReceiptPictureHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace HsaDotnetBackend.Helpers
+{
+    public class ReceiptPictureHelper
+    {
+        public static string GetReceiptPictureUri(string receiptBlobId, int uriValidForMinutes = 30)
+        {
+            CloudStorageAccount storageAccount =
+                CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = 
+                blobClient.GetContainerReference(ConfigurationManager.AppSettings["ReceiptContainer"]);
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(receiptBlobId);
+
+            SharedAccessBlobPolicy policy = new SharedAccessBlobPolicy()
+            {
+                SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-5),
+                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(uriValidForMinutes),
+                Permissions = SharedAccessBlobPermissions.Read
+            };
+
+            string sasBlobToken = blockBlob.GetSharedAccessSignature(policy);
+
+            return blockBlob.Uri + sasBlobToken;
+        }
+    }
+}
