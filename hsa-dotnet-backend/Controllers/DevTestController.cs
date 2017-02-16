02614767b782d63a59573a470cc7873815d8ba8a
Pearse Hutson <pearse.2008@gmail.com>
2017-02-16 14:54:23 -0600

    Blob testing on DevTest

--- hsa-dotnet-backend/Controllers/DevTestController.cs
+++ hsa-dotnet-backend/Controllers/DevTestController.cs
@@ -4,6 +4,9 @@ using System.Web.Http;
 using AutoMapper;
 using HsaDotnetBackend.Models;
 using HsaDotnetBackend.Models.DTOs;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace HsaDotnetBackend.Controllers
 { 
@@ -33,5 +36,30 @@ namespace HsaDotnetBackend.Controllers
         //{
         //    var products = context
         //}
+
+        [Route("devtest/blob")]
+        [HttpGet]
+        public string GetBlob()
+        {
+            CloudStorageAccount storageAccount =
+                CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference("userreceipts");
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("20170216_140140[1].jpg");
+
+            SharedAccessBlobPolicy policy = new SharedAccessBlobPolicy()
+            {
+                SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-5),
+                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(30),
+                Permissions = SharedAccessBlobPermissions.Read
+            };
+
+            string sasBlobToken = blockBlob.GetSharedAccessSignature(policy);
+
+            return blockBlob.Uri + sasBlobToken;
+        }
     }
 }
