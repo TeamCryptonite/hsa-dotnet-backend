00e0e510415539985392836af55bf49739d7b31d
Pearse Hutson <pearse.2008@gmail.com>
2017-02-18 15:21:00 -0600

    Installed Microsoft.AspNet.OData

--- hsa-dotnet-backend/Controllers/ReceiptsController.cs
+++ hsa-dotnet-backend/Controllers/ReceiptsController.cs
@@ -9,6 +9,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using System.Web.OData;
 using System.Web.Services.Description;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -23,6 +24,7 @@ namespace HsaDotnetBackend.Controllers
         private Fortress_of_SolitudeEntities db = new Fortress_of_SolitudeEntities();
 
         // GET: api/Receipts
+        [EnableQueryAttribute]
         public IQueryable<ReceiptDto> GetReceipts(int skip = 0, int take = 10)
         {
             var userGuid = IdentityHelper.GetCurrentUserGuid();
