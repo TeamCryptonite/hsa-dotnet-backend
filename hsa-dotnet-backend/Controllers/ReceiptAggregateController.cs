9d2d08502f172c7a08159310340ef476e0efaa0e
PercyODI <pearse.2008@gmail.com>
2017-02-20 20:01:27 -0600

    Testing aggregate data endpoint

--- hsa-dotnet-backend/Controllers/ReceiptAggregateController.cs
+++ hsa-dotnet-backend/Controllers/ReceiptAggregateController.cs
@@ -17,7 +17,7 @@ namespace HsaDotnetBackend.Controllers
         // TODO: Create spendingovertimedto
         [HttpGet]
         [Route("api/receiptaggregate/spendingovertime")]
-        public async Task<object> SpendingOverTime(string startDateStr, string endDateStr)
+        public async Task<object> SpendingOverTime(string startDateStr = null, string endDateStr = null, string timePeriod = "yearmonth")
         {
             // Authorize user
             var userGuid = IdentityHelper.GetCurrentUserGuid();
@@ -28,14 +28,42 @@ namespace HsaDotnetBackend.Controllers
             DateTime startDate = startDateStr != null ? DateTime.Parse(startDateStr) : DateTime.Now.AddMonths(-6);
             DateTime endDate = endDateStr != null ? DateTime.Parse(endDateStr) : DateTime.Now;
 
-            var receipts = db.Receipts
+            // Parse timePeriod
+            string dateTimeGroupFormat;
+
+            switch (timePeriod)
+            {
+                case "yearmonth":
+                    dateTimeGroupFormat = "y"; // "March, 2008" YearMonth
+                    break;
+                case "day":
+                    dateTimeGroupFormat = "d"; // "3/9/2008" ShortDate;
+                    break;
+                case "month":
+                    dateTimeGroupFormat = "MMMM"; // "March" Month full name
+                    break;
+                case "year":
+                    dateTimeGroupFormat = "yyyy"; // "2017" full year
+                    break;
+                default:
+                    dateTimeGroupFormat = "y";
+                    break;
+            }
+
+            var agregateData = db.Receipts
                 .Where(r => r.UserObjectId == userGuid)
                 .Where(r => r.DateTime >= startDate)
                 .Where(r => r.DateTime <= endDate)
-                .GroupBy(r => r.DateTime.Value.Month);
+                .GroupBy(r => r.DateTime.Value.ToString(dateTimeGroupFormat))
+                .Select(group => new
+                {
+                    TotalSpent = group.Sum(r => r.LineItems.Sum(li => li.Price)),
+                    TotalHsaSpent = group.Sum(r => r.LineItems.Where(li => li.Product.IsHSA).Sum(li => li.Price)),
+                    ProductList = group.SelectMany(r => r.LineItems).Select(li => new {Product = li.Product.Name}).Distinct()
+                });
 
 
-            return new {};
+            return agregateData;
         }
 
     }
