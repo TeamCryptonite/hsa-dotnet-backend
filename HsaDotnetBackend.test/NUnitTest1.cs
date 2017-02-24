03f83af061bac0ab4989c7b9eb8786ccbc82e912
Pearse Hutson <pearse.2008@gmail.com>
2017-02-24 13:36:52 -0600

    Installed Moq

---
+++ HsaDotnetBackend.test/NUnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HsaDotnetBackend.test
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        //[Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ hsa-dotnet-backend.Tests/TestReceiptController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hsa_dotnet_backend.Tests
+{
+    [TestClass]
+    public class TestReceiptController
+    {
+        [TestMethod]
+        public void GetReceipts_ShouldReturnAllUserReceipts()
+        {
+
+        }
+    }
+}
